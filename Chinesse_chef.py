d3a17a7f888ce75d58b2139cc60e182a7e3ac96c
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-13 15:39:18 +0200

    work with two classes in app

---
+++ Chinesse_chef.py
@@ -0,0 +1,8 @@
+
+class Chinesse_chef:
+
+    def chicken(self):
+        print("Chef makes the chicken with rice")
+
+    def breadcrumbs(self):
+        print("Chef makes the chicken in breadcrumbs ")
--- app_chef.py
+++ app_chef.py
@@ -1,5 +1,9 @@
 
 from test_def import Chef
+from Chinesse_chef import Chinesse_chef
 
 myChef = Chef()
 myChef.make_special_dish()
+
+myChinesseChef = Chinesse_chef()
+myChinesseChef.chicken()
